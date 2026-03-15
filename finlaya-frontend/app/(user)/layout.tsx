9299d946b65ec9e8d2df51b7a7de9f69d1b8d4e0
Suyog0-0 <suyogsigdel000@gmail.com>
2026-03-15 14:42:00 +0545

    add NotificationProvider and ToastContainer to user layout

--- finlaya-frontend/app/(user)/layout.tsx
+++ finlaya-frontend/app/(user)/layout.tsx
@@ -1,10 +1,13 @@
 import Navbar from "@/components/layout/navbar";
+import { NotificationProvider } from "@/lib/contexts/NotificationContext";
+import ToastContainer from "@/components/layout/ToastContainer";
 
 export default function UserLayout({ children }: { children: React.ReactNode }) {
   return (
-    <>
+    <NotificationProvider>
       <Navbar />
+      <ToastContainer />
       <main>{children}</main>
-    </>
+    </NotificationProvider>
   );
 }
